ce1c78d281db8ef880163ab33b5f86395a09a9d2
Anas Ansari <121580678+annasansari@users.noreply.github.com>
2024-05-22 13:04:04 +0500

    change URI

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const axios = require('axios');
 
 const app = express();
 
-const MONGO = 'mongodb+srv://anas:anas@tweetserver.rhvsnwp.mongodb.net/tweetDatabase?retryWrites=true&w=majority&appName=tweetServer'
+const MONGO = 'mongodb+srv://hassanzindani030:ojvYwKeiRPd3pShX@cluster0.1sbjnwu.mongodb.net/tweetDatabase?retryWrites=true&w=majority&appName=Cluster0'
 
 mongoose.connect(MONGO).then(() => {
     console.log("DB connected");
